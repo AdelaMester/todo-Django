31c8dce1fd1e5a6905a042aea2cd57a417c91365
AdelaMester <adelamester@yahoo.co.uk>
2022-03-05 15:45:17 +0000

    fixed library mismatch

--- todoList/urls.py
+++ todoList/urls.py
@@ -1,12 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path, include
 from . import views
-from django.conf.urls import url
 from django.conf import settings
 from django.views.static import serve
 
 urlpatterns = [
     path("", views.index, name="index"),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
