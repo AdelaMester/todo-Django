4212554fd2a759c79a3859081aec00289731238a
AdelaMester <adelamester@yahoo.co.uk>
2022-03-05 17:07:58 +0000

    Login

--- todoList/urls.py
+++ todoList/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
