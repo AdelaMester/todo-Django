86e756573ab1347fab61faa9040e80d137ba1fd4
AdelaMester <adelamester@yahoo.co.uk>
2022-03-05 15:26:27 +0000

    Fixed collectstatic errors

--- todoList/urls.py
+++ todoList/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path("", views.index, name="index"),
