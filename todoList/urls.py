2d1909c66858f0dcbaf3450f64b984e98b40860e
AdelaMester <adelamester@yahoo.co.uk>
2022-03-05 17:29:29 +0000

    Created home route

--- todoList/urls.py
+++ todoList/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('accounts/', include('django.contrib.auth.urls')),
+    path("home/", views.home, name="home"),
 ]
 
