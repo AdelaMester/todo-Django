647b759aed8cb8e8285d8c0d40ebe5dfa10c399a
AdelaMester <adelamester@yahoo.co.uk>
2022-03-05 17:40:16 +0000

    Created login route

--- todoList/views.py
+++ todoList/views.py
@@ -6,13 +6,6 @@ from django.http import HttpResponse
 def index(request):
     return render(request, "todoList/index.html")
 
-def home(request):
+def request_identity(request):
     if request.method == 'GET':
-
-        # Login validation, if there is no session['name'] redirect to / route
-        if 'name' not in request.session:
-            return HttpResponseRedirect("/")
-        return render(request, "todoList/home.html",{
-            'name': request.session['name']
-
-        })
+        return render(request, "todoList/login.html")
