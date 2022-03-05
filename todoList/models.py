ac179fa76548f6650ae4c51c57abc0feebec7879
AdelaMester <adelamester@yahoo.co.uk>
2022-03-05 21:12:43 +0000

    register logic created

--- todoList/models.py
+++ todoList/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100)
+    hash_password = models.CharField(max_length=200)
+
+class Tasks(models.Model):
+    task_id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=100)
+    completed = models.CharField(max_length=10)
--- todoList/views.py
+++ todoList/views.py
@@ -13,5 +13,42 @@ def login(request):
         return render(request, "todoList/login.html")
 
 def register(request):
+
+    """ Register user """
+
     if request.method == 'GET':
         return render(request, "todoList/register.html")
+
+    if request.method == 'POST':
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return HttpResponse("Must provide username", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return HttpResponse("Must provide password", 400)
+
+        #Ensure field is not empty
+        elif not request.form.get("confirmation"):
+            return HttpResponse("Must confirm password", 400)
+
+        #Ensure password is matching
+        elif not (request.form.get("confirmation") == request.form.get("password")):
+            return HttpResponse("Password is not matching", 400)
+
+        #Ensure there is no duplicate for usename
+        existing_user=request.form.get("username")
+        password=request.form.get("password")
+        old_user = Users.objects.get(username = existing_user)
+
+        # if the username does not exist in the db, it will be inserted
+        if len(old_user) == 0:
+            user = Users(existing_user, generate_password_hash (password, method='pbkdf2:sha256', salt_length=8))
+        else:
+            return HttpResponse("User name not available",400)
+
+        return render(request,"todoList/registered.html", user_name=existing_user)
+
+
+
