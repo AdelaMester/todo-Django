6ea41f167546ad69766e6bf044473e052b17e967
AdelaMester <adelamester@yahoo.co.uk>
2022-03-05 16:30:54 +0000

    Added templates and basic style to the page

--- todoList/views.py
+++ todoList/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello there!")
+    return render(request, "todoList/index.html")
+
+def home(request):
+    if request.method == 'GET':
+
+        # Login validation, if there is no session['name'] redirect to / route
+        if 'name' not in request.session:
+            return HttpResponseRedirect("/")
+        return render(request, "todoList/home.html",{
+            'name': request.session['name']
+
+        })
