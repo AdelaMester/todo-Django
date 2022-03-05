73bbc3185bbda96be6ff26944977ddc6a5512f0e
AdelaMester <adelamester@yahoo.co.uk>
2022-03-05 17:48:35 +0000

    Fix bugs to render template

--- todoList/urls.py
+++ todoList/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('accounts/', include('django.contrib.auth.urls')),
-    path("request_identity/", views.request, name="request_identity"),
+    path("request_identity/", views.request_identity, name="request_identity"),
 ]
 
