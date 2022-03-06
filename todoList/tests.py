2137eb206f4558c08dd4a0b1efecf7646e651d69
AdelaMester <adelamester@yahoo.co.uk>
2022-03-06 15:34:45 +0000

    Added test case

--- todoList/tests.py
+++ todoList/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestCases(TestCase):
+    def test_ok(self):
+        response = self.client.get('/')
+        print("code: "+ str(response.status_code))
+        self.assertEqual(response.status_code, 200)
+
